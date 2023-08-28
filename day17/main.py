b80f285611d49f0deaf008fc4b18516253e48e93
unknown <jackw1689@gmail.com>
2023-08-28 16:00:22 -0700

    day17

---
+++ day17/main.py
@@ -0,0 +1,56 @@
+from quiz_brain import QuizBrain
+import os
+
+
+def clear_console():
+    os.system('cls')
+
+
+
+
+class Game:
+    def __init__(self):
+        self.chances = 10
+        self.quiz_brain = QuizBrain()
+        self.score = 0
+
+    def reset_score(self):
+        clear_console()
+        self.chances = 10
+        self.score = 0
+
+    def initialize_game(self):
+        play_again = True
+
+        while play_again:
+
+            self.quiz_brain.load_question()
+            self.quiz_brain.ask_question()
+            if self.quiz_brain.check_answer():
+                self.score += 1
+                print(f'\nCorrect!! Score: {self.score}/10\n')
+                self.chances -= 1
+            else:
+                print(f'\nIncorrect. Score: {self.score}/10\n')
+                self.chances -= 1
+
+            if self.chances == 0:
+                while True:
+                    print(f'FINAL SCORE: {self.score}/10\n')
+                    selection = input('Play again? (y)es or (n)o')
+                    if selection == 'y':
+                        self.reset_score()
+                        break
+
+                    elif selection == 'n':
+                        play_again = False
+                        break
+                    else:
+                        print('Please select a valid option (y)es or (no ')
+
+        print('Thanks for playing!')
+
+
+game = Game()
+
+game.initialize_game()
---
+++ day17/quiz_brain.py
@@ -0,0 +1,26 @@
+from question_model import Question
+from data import question_data
+import random
+
+question_bank = []
+
+for item in question_data:
+    question_bank.append(Question(item['text'], item['answer']))
+
+
+class QuizBrain:
+    def __init__(self):
+        self.question = None
+        self.player_choice = ''
+
+    def load_question(self):
+        self.question = random.choice(question_bank)
+
+    def ask_question(self):
+        print(f'{self.question.text}\n')
+        self.player_choice = input(f'True or False?  ')
+
+    def check_answer(self):
+        if self.player_choice.lower() == self.question.answer.lower():
+            return True
+        return False
